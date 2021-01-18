a450e71cc5ad4afc3d68269e16e401318f4a5f67
PaoManrique <paomanrique03@gmail.com>
2021-01-19 01:16:02 +0200

    Mandatory

---
+++ week3/day3/favorite_color.js
@@ -0,0 +1,11 @@
+
+// Write a simple JavaScript program to join all elements of the following array into a string.
+
+// let me = ["my","favorite","color","is","blue"]
+// console.log(me.toString());
+// console.log(me.join());
+
+let me = ["my","favorite","color","is","blue"]
+let arrayTostring = me.join(" "); 
+console.log(arrayTostring);
+
