71c4514f33b86977e89a42013bd9110ca711ce1c
PaoManrique <paomanrique03@gmail.com>
2021-01-30 22:25:20 +0200

    modified

--- week4/day2/words_stars.js
+++ week4/day2/words_stars.js
@@ -1,13 +1,25 @@
-var word = prompt("Write something separated by commas");
-    wordArray = word.split(' ').join(" ");
-    console.log(wordArray);
+function frame(){
+    let words = prompt("Please write some words separated by commas");
+    words = words.split(",").map(item => item.trim());
+    
+    let longest = '';
 
-function frame(str){
-    var border=""; 
-    for(i=0;i<str.length+4;i++){
-     border += "*" 
+    for( word of words){
+        if(word.length > longest.length){
+            longest = word;
+        }
+
     }
-    return border + "\n* "+str+" *\n" + border;
+    console.log("*".repeat(longest.length + 4));
+
+    for (w of words){
+        let space_W = (" ".repeat(longest.length - w.length));
+
+        console.log("* " + w + space_W + " *");
+    }
+
+    console.log("*".repeat(longest.length + 4));        
 
 }
-console.log(frame(word));
+
+frame();
