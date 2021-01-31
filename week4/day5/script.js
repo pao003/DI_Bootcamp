39bb7a3e2ef87a870ca47aad8c71d43bf7eb7724
PaoManrique <paomanrique03@gmail.com>
2021-01-31 19:57:01 +0200

    changes

--- week4/day5/script.js
+++ week4/day5/script.js
@@ -1,24 +1,39 @@
-var planets = [
-		        ["Mercury", "46.3, 46.3, 46.3"],
-		        ["Venus", "80.4, 78.4, 76.1"], 
-		        ["Earth", "34.9, 37.3, 51.4"], 
-		        ["Mars", "99.6, 52.9, 37.3"],
-		        ["Jupiter", "85.1, 74.9, 65.1"], 
-		        ["Saturn", "86.3, 73.7, 50.6"],
-		        ["Uranus", "76.5, 91.4, 92.5"],
-		        ["Neptune", "28.2, 47.5, 98.8"]
-	    ];
+let solarSystem = [
+        { 'name': 'Mercury', 'moons': 0, 'color': 'orange'},
+        { 'name': 'Venus', 'moons': 0, 'color': 'grey'},
+        { 'name': 'Earth', 'moons': 1, 'color': 'blue'},
+        { 'name': 'Mars', 'moons': 2, 'color': 'red'},
+        { 'name': 'Jupiter', 'moons': 79, 'color': 'brown'},
+        { 'name': 'Saturn', 'moons': 62, 'color': 'yellow'},
+        { 'name': 'Uranus', 'moons': 27, 'color': 'lightblue'},
+        { 'name': 'Neptune', 'moons': 14, 'color': 'darkblue'},
 
-	    for (var i = 0; i < planets.length; i++) {
+        ]
 
-	        var planetNode = document.createElement("div");
-	        planetNode.style.backgroundColor = "rgb(" + planets[i][1] + ")";
-	        planetNode.className = "planet";
+        for(planet of solarSystem){
+            let planetDiv = document.createElement('div');
 
-	        var nameNode = document.createTextNode(planets[i][0]);
-	        planetNode.appendChild(nameNode);
+            planetDiv.classList.add('planet');
+            planetDiv.innerHTML = planet.name;
 
-	        var body = document.querySelector("body");
-	        body.appendChild(planetNode);
-	    }
-	    
+            let i = 1;
+            let moonDiv;
+
+            while (i <= planet.moons){
+
+                moonDiv = document.createElement('div');
+                moonDiv.classList.add('moon');
+
+                let moveMoon = (i * 30) + 90 + 'px';
+                moonDiv.style.left = moveMoon;
+
+                moonDiv.style.backgroundColor = 'white';
+
+                planetDiv.appendChild(moonDiv);
+
+                i++;
+            }
+
+            planetDiv.style.backgroundColor = planet.color;
+            document.body.appendChild(planetDiv);
+        }
